e2cbb2e5cc8f2f323dd03c5620101bf479ba7e84
Dat_Andre <junojunojunojuno@proton.me>
2022-11-09 16:52:36 +0000

    lesson_08_11_2022

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cw_template_break_full::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
